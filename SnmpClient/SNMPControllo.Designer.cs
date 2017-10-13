f6aeea5465ec28ff7aef9e7e357abf33ad1fc29c
Fabia Ferrero <fabiaferrero@gmail.com>
2017-10-13 15:32:01 +0200

    Funzionante, con modifica gui, scrive su database nome stampante e copie e IP

---
+++ SnmpClient/SNMPControllo.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SnmpClient
+{
+    partial class SNMPControllo
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckStampante = new System.Windows.Forms.Button();
+            this.discoverBotton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CheckStampante
+            // 
+            this.CheckStampante.AutoSize = true;
+            this.CheckStampante.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.CheckStampante.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CheckStampante.FlatAppearance.BorderSize = 0;
+            this.CheckStampante.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CheckStampante.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckStampante.Location = new System.Drawing.Point(12, 50);
+            this.CheckStampante.Name = "CheckStampante";
+            this.CheckStampante.Size = new System.Drawing.Size(150, 25);
+            this.CheckStampante.TabIndex = 0;
+            this.CheckStampante.Text = "Check Stampante";
+            this.CheckStampante.UseVisualStyleBackColor = false;
+            this.CheckStampante.Click += new System.EventHandler(this.CheckStampanteBotton_Click);
+            // 
+            // discoverBotton
+            // 
+            this.discoverBotton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.discoverBotton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.discoverBotton.FlatAppearance.BorderSize = 0;
+            this.discoverBotton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.discoverBotton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.discoverBotton.Location = new System.Drawing.Point(12, 21);
+            this.discoverBotton.Name = "discoverBotton";
+            this.discoverBotton.Size = new System.Drawing.Size(150, 23);
+            this.discoverBotton.TabIndex = 6;
+            this.discoverBotton.Text = "Discover Stampanti";
+            this.discoverBotton.UseVisualStyleBackColor = false;
+            this.discoverBotton.Click += new System.EventHandler(this.discoverBotton_ClickAsync);
+            // 
+            // SNMPControllo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 298);
+            this.Controls.Add(this.discoverBotton);
+            this.Controls.Add(this.CheckStampante);
+            this.Name = "SNMPControllo";
+            this.Text = "SNMP Controllo Stampante";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckStampante;
+        private System.Windows.Forms.Button discoverBotton;
+    }
+}
+
---
+++ SnmpClient/SNMPControllo.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using SnmpSharpNet;
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+
+namespace SnmpClient
+{
+    public partial class SNMPControllo : Form
+    {
+        SnmpSharpNet.OctetString community;
+        AgentParameters param;
+        IpAddress agent;
+        UdpTarget target;
+        private SNMPEntities SNMPContext;
+        int id = 0;
+
+        public SNMPControllo()
+        {
+            InizializzaSNMP();
+            InitializeComponent();
+        }
+
+        private void InizializzaSNMP()
+        {
+            community = new SnmpSharpNet.OctetString("public");                   //SNMP community name, di default "public"
+            param = new AgentParameters(community);                               //Definisce parametri dell'agente, secondo community name
+            param.Version = SnmpVersion.Ver1;                                     //Definizione versione SNMP 1
+            agent = new IpAddress("192.168.1.96");                                //Definizione indirizzo IP della macchina
+            target = new UdpTarget((IPAddress)agent, 161, 2000, 1);               //Definizione metodo con cui comunicare con la stampante: UDP, porta, timeout, retry
+        }
+
+
+        private void CheckStampanteBotton_Click(object sender, EventArgs e)
+        {
+            Pdu pdu = new Pdu(PduType.Get);                                                 //Unità dati dello scambio messaggi SNMP
+            //pdu.VbList.Add("1.3.6.1.2.1.1.1.0");                                            //Definizione generale dispositivo
+            //pdu.VbList.Add("1.3.6.1.2.1.13.0");                                             //Tempo attività
+            pdu.VbList.Add("1.3.6.1.2.1.1.5.0");                                            //Nome dipositivo
+            pdu.VbList.Add("1.3.6.1.4.1.367.3.2.1.2.19.1.0");                               //Stampe totali complete
+            //pdu.VbList.Add(".1.3.6.1.4.1.367.3.2.1.2.19.2.0");                              //Stampe totali complete Stampante
+            //pdu.VbList.Add(".1.3.6.1.4.1.367.3.2.1.2.19.4.0");                              //Stampe totali complete Fotocopiatrice
+            //pdu.VbList.Add(".1.3.6.1.2.1.3.1.1.2.1.1.192.168.1.32");                        //Indirizzo MAC
+            //pdu.VbList.Add("1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.4");                           //Yellow
+            //pdu.VbList.Add("1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.3");                           //Magenta
+            //pdu.VbList.Add("1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.2");                           //Ciano
+            //pdu.VbList.Add("1.3.6.1.4.1.367.3.2.1.2.24.1.1.5.1");                           //Black
+
+            SnmpV1Packet result = (SnmpV1Packet)target.Request(pdu, param);                 //Richiesta SNMP
+
+            if (result != null)
+            {
+                if (result.Pdu.ErrorStatus != 0)
+                {
+                    Console.WriteLine("Errore durante la richiesta SNMP. Errore {0} index {1}", result.Pdu.ErrorStatus, result.Pdu.ErrorIndex);
+                }
+                else
+                {
+                    //Console.WriteLine("Dispositivo: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[0].Value.Type), result.Pdu.VbList[0].Value.ToString());
+                    //Console.WriteLine("Tempo Attività: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[1].Value.Type), result.Pdu.VbList[1].Value.ToString());
+                    Console.WriteLine("Nome Dispositivo: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[0].Value.Type), result.Pdu.VbList[0].Value.ToString());
+                    Console.WriteLine("Stampe totali complete: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[1].Value.Type), result.Pdu.VbList[1].Value.ToString());
+                    //Console.WriteLine("Stampe totali complete Stampante: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[4].Value.Type), result.Pdu.VbList[4].Value.ToString());
+                    //Console.WriteLine("Stampe totali complete Fotocopiatrice: ({0}): {1}", SnmpConstants.GetTypeName(result.Pdu.VbList[5].Value.Type), result.Pdu.VbList[5].Value.ToString());
+
+
+                    SNMPContext = new SNMPEntities();
+
+
+                    Stampanti risultatoSNMP = new Stampanti
+                    {
+                        IDStampante = id++,
+                        Nome = result.Pdu.VbList[0].Value.ToString(),
+                        IP = agent.ToString(),
+                        NumeroStampe = Int32.Parse(result.Pdu.VbList[1].Value.ToString()),
+                        Data = DateTime.Now
+                    };
+
+                    try
+                    {
+                        SNMPContext.Stampantis.Add(risultatoSNMP);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Errore nell'inserimento dei dati "+ex);  
+                    }
+                    SNMPContext.SaveChanges();
+                    MessageBox.Show("Elementi salvati nel database.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nessuna risposta dall'agente SNMP");
+            }
+
+        }
+
+
+        private async void discoverBotton_ClickAsync(object sender, EventArgs e) //controlla la rete locale per individuare le stampanti. Sono individuate tramite risposta al broadcast
+        {
+            Discoverer discoverer = new Discoverer();
+            discoverer.AgentFound += DiscovererAgentFound;
+            Console.WriteLine("v1 discovery");
+            await discoverer.DiscoverAsync(VersionCode.V1, new IPEndPoint(IPAddress.Broadcast, 161), new Lextm.SharpSnmpLib.OctetString("public"), 6000);
+        }
+
+        static void DiscovererAgentFound(object sender, AgentFoundEventArgs e)
+        {
+            Console.WriteLine("Dispositivo Trovato IP:{0}--{1}", e.Agent, e.Variable); //Dispositivo trovato nella rete
+        }
+
+
+    }
+}
